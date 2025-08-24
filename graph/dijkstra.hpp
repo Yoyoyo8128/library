e9c67e06b00bd256e8932cd152a23dacec686f2a
root <root@Yoyoyo8128.localdomain>
2025-08-24 09:58:21 +0900

    update dijkstra

---
+++ graph/dijkstra.hpp
@@ -0,0 +1,33 @@
+// ダイクストラ
+struct Edge
+{
+    long long to;
+    long long cost;
+};
+void dijkstra(const vector<vector<Edge>> &G, int s, vector<long long> &dis)
+{
+    int N = G.size();
+    dis.resize(N, inf);
+    priority_queue<pair<long long, int>, vector<pair<long long, int>>, greater<pair<long long, int>>> pq; // 「仮の最短距離, 頂点」が小さい順に並ぶ
+    dis[s] = 0;
+    pq.emplace(dis[s], s);
+    while (!pq.empty())
+    {
+        pair<long long, int> p = pq.top();
+        pq.pop();
+        int v = p.second;
+        if (dis[v] < p.first)
+        { // 最短距離で無ければ無視
+            continue;
+        }
+        for (auto &e : G[v])
+        {
+            if (dis[e.to] > dis[v] + e.cost)
+            { // 最短距離候補なら priority_queue に追加
+                dis[e.to] = dis[v] + e.cost;
+                pq.emplace(dis[e.to], e.to);
+            }
+        }
+    }
+}
+// https://algo-logic.info/dijkstra/
