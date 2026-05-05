6f3b26a623fdbf4b21f0f7958c87975a56819ba6
root <root@Yoyoyo8128.localdomain>
2026-05-05 23:00:20 +0900

    bellman bool値返す

--- graph/bellman.hpp
+++ graph/bellman.hpp
@@ -1,35 +1,33 @@
-struct Edge
-{
+struct Edge{
     int to;
     long long cost;
 };
-bool bellman(const vector<vector<Edge>> &G, int s, vector<long long> &dis){
-    
 
+pair<bool,vector<ll>> bellman(const vector<vector<Edge>>&G,int s){
     int N = G.size();
-    dis.resize(N,inf);
+    vector<ll> dis(N, inf);
+    dis[s] = 0;
 
-    dis[s]=0;
-    
-    for(int i=0;i<N-1;i++){
-        for(int j=0;j<N;j++){
+    for(int i = 0; i < N - 1; i++){
+        for(int j = 0; j < N; j++){
             if(dis[j] == inf) continue;
-            for(Edge e:G[j]){
-                chmin(dis[e.to],dis[j]+e.cost);
+            for(Edge e : G[j]){
+                chmin(dis[e.to], dis[j] + e.cost);
             }
         }
-        //print(dis);
     }
-    vector<ll>dis2=dis;
-    for(int j=0;j<N;j++){
-        if(dis[j] == inf) continue;
-        for(Edge e:G[j]){
-            chmin(dis2[e.to],dis2[j]+e.cost);
+    
+    vector<ll> dis2 = dis;
+    for(int j = 0; j < N; j++){
+        if(dis2[j] == inf) continue;
+        for(Edge e : G[j]){
+            chmin(dis2[e.to], dis2[j] + e.cost);
         }
     }
-    if(dis2!=dis){
-        return false;//負閉路
+
+    if(dis2 != dis){
+        return make_pair(false,vector<ll>());
     }else{
-        return true;
+        return make_pair(true,dis);
     }
 }
