53d058d6b8fb9f66b1bb5e55ee2be36babdef81a
root <root@Yoyoyo8128.localdomain>
2026-05-05 22:36:14 +0900

    dijkstra

--- graph/bellman.hpp
+++ graph/bellman.hpp
@@ -1,18 +1,19 @@
 struct Edge
 {
-    long long to;
+    int to;
     long long cost;
 };
-void bellman(const vector<vector<Edge>> &G, int s, vector<long long> &dis){
+bool bellman(const vector<vector<Edge>> &G, int s, vector<long long> &dis){
     
 
     int N = G.size();
-    dis.resize(N,inf);//print(dis);
-    //print(N);
+    dis.resize(N,inf);
+
     dis[s]=0;
     
     for(int i=0;i<N-1;i++){
         for(int j=0;j<N;j++){
+            if(dis[j] == inf) continue;
             for(Edge e:G[j]){
                 chmin(dis[e.to],dis[j]+e.cost);
             }
@@ -21,11 +22,14 @@ void bellman(const vector<vector<Edge>> &G, int s, vector<long long> &dis){
     }
     vector<ll>dis2=dis;
     for(int j=0;j<N;j++){
+        if(dis[j] == inf) continue;
         for(Edge e:G[j]){
             chmin(dis2[e.to],dis2[j]+e.cost);
         }
     }
     if(dis2!=dis){
-        dis[s]=-114514;
+        return false;//負閉路
+    }else{
+        return true;
     }
 }
--- graph/dijkstra.hpp
+++ graph/dijkstra.hpp
@@ -1,32 +1,26 @@
-// ダイクストラ
 struct Edge
 {
     int to;
     long long cost;
 };
-void dijkstra(const vector<vector<Edge>> &G, int s, vector<long long> &dis)
+vector<long long> dijkstra(const vector<vector<Edge>> &G, int s)
 {
     int N = G.size();
-    dis.resize(N, inf);
+    vector<ll>dis(N,inf);
     priority_queue<pair<long long, int>, vector<pair<long long, int>>, greater<pair<long long, int>>> pq;
-    dis[s] = 0;
-    pq.emplace(dis[s], s);
-    while (!pq.empty())
-    {
-        pair<long long, int> p = pq.top();
+    dis[s]=0;
+    pq.emplace(dis[s],s);
+    while(!pq.empty()){
+        pair<long long,int>tmp=pq.top();
         pq.pop();
-        int v = p.second;
-        if (dis[v] < p.first)
-        { 
-            continue;
-        }
-        for (auto &e : G[v])
-        {
-            if (dis[e.to] > dis[v] + e.cost)
-            {
-                dis[e.to] = dis[v] + e.cost;
-                pq.emplace(dis[e.to], e.to);
+        int v=tmp.second;
+        if(dis[v]<tmp.first)continue;
+        for(auto &e:G[v]){
+            if(dis[e.to]>dis[v] + e.cost){
+                dis[e.to]=dis[v] + e.cost;
+                pq.emplace(dis[e.to],e.to);
             }
         }
     }
+    return dis;
 }
