4f6489e23ad8277b1d51e7c51fdaab0a5fb22d23
root <root@Yoyoyo8128.localdomain>
2026-05-06 02:18:41 +0900

    rolling hash

--- string/rollinghash.hpp
+++ string/rollinghash.hpp
@@ -1,75 +1,66 @@
-template< unsigned mod >
 struct RollingHash {
-  vector< unsigned > hashed, power;
+    using ull = unsigned long long;
+    static const ull mod = (1ULL << 61) - 1;
 
-  inline unsigned mul(unsigned a, unsigned b) const {
-    unsigned long long x = (unsigned long long) a * b;
-    unsigned xh = (unsigned) (x >> 32), xl = (unsigned) x, d, m;
-    asm("divl %4; \n\t" : "=a" (d), "=d" (m) : "d" (xh), "a" (xl), "r" (mod));
-    return m;
-  }
+    static const ull base = 20090926;//My Birthday.
 
-  RollingHash(const string &s, unsigned base = 10007) {
-    int sz = (int) s.size();
-    hashed.assign(sz + 1, 0);
-    power.assign(sz + 1, 0);
-    power[0] = 1;
-    for(int i = 0; i < sz; i++) {
-      power[i + 1] = mul(power[i], base);
-      hashed[i + 1] = mul(hashed[i], base) + s[i];
-      if(hashed[i + 1] >= mod) hashed[i + 1] -= mod;
-    }
-  }
+    vector<ull> power;
+    vector<ull> hash;
 
-  unsigned get(int l, int r) const {
-    unsigned ret = hashed[r] + mod - mul(hashed[l], power[r - l]);
-    if(ret >= mod) ret -= mod;
-    return ret;
-  }
-
-  unsigned connect(unsigned h1, int h2, int h2len) const {
-    unsigned ret = mul(h1, power[h2len]) + h2;
-    if(ret >= mod) ret -= mod;
-    return ret;
-  }
-
-  int LCP(const RollingHash< mod > &b, int l1, int r1, int l2, int r2) {
-    int len = min(r1 - l1, r2 - l2);
-    int low = -1, high = len + 1;
-    while(high - low > 1) {
-      int mid = (low + high) / 2;
-      if(get(l1, l1 + mid) == b.get(l2, l2 + mid)) low = mid;
-      else high = mid;
-    }
-    return (low);
-  }
-};
-
-using RH = RollingHash< 1000000007 >;
-
-
-/*
-int main(){
-     string s = "abracadabra";
-    RH rh(s);
-
-    // 部分文字列 s[0:3] ("abr") のハッシュ値
-    ll h1 = rh.get(0, 3);
-    cout << "hash(\"abr\") = " << h1 << endl;
-
-    // 部分文字列 s[7:11] ("abra") のハッシュ値
-    ll h2 = rh.get(7, 11);
-    cout << "hash(\"abra\") = " << h2 << endl;
-
-    // ハッシュ値比較（部分文字列の一致判定）
-    if (h1 == rh.get(7, 10)) {
-        cout << "s[0:3] == s[7:10]" << endl;
+    static ull mul(ull a, ull b) {
+        __uint128_t t = (__uint128_t)a * b;
+        t = (t >> 61) + (t & mod);
+        if (t >= mod) t -= mod;
+        return (ull)t;
     }
 
-    // 別の文字列との LCP
-    string t = "abracad";
-    RH rh2(t);
-    int lcp_len = rh.LCP(rh2, 0, s.size(), 0, t.size());
-    cout << "LCP(s, t) length = " << lcp_len << endl;
+    static ull add(ull a, ull b) {
+        a += b;
+        if (a >= mod) a -= mod;
+        return a;
+    }
+
+    static ull sub(ull a, ull b) {
+        a += mod - b;
+        if (a >= mod) a -= mod;
+        return a;
+    }
+
+    RollingHash(const string &s) {
+        int n = s.size();
+        power.resize(n + 1);
+        hash.resize(n + 1);
+
+        power[0] = 1;
+        for (int i = 0; i < n; i++) {
+            power[i + 1] = mul(power[i], base);
+        }
+
+        hash[0] = 0;
+        for (int i = 0; i < n; i++) {
+            hash[i + 1] = add(mul(hash[i], base), s[i]+1);
+        }
+    }
+
+    //[l, r)
+    ull get(int l, int r) const {
+        return sub(hash[r], mul(hash[l], power[r - l]));
+    }
+
+    // s[i..] と s[j..] のLCP
+    int lcp(int i, int j) const {
+        int n = hash.size() - 1;
+        int low = 0;
+        int high = min(n - i, n - j) + 1;
     
-}*/
+        while (high - low > 1) {
+            int mid = (low + high) / 2;
+            if (get(i, i + mid) == get(j, j + mid)) {
+                low = mid;
+            } else {
+                high = mid;
+            }
+        }
+        return low;
+    }
+};
